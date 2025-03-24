007cfed3f26b9141176b9eedb8855f4ac10169ce
JorginhoVigas <jorginho.vigas31@gmail.com>
2025-03-24 17:06:37 -0300

    novos-assuntos-aula-dio

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,42 @@
+public class Operadores {
+    public static void main(String[] args) {
+        boolean condicao1=true;
+        
+        boolean condicao2=true;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condicoes sao verdadeiras");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condicoes é verdadeira");
+        }
+        System.out.println("Fim");
+        
+        
+        
+        /*String nomeUm = "Jorge";
+        String nomeDois = new String("Jorge");
+        
+        System.out.println(nomeUm.equals(nomeDois));
+        
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 == numero2){//true
+            System.out.println("a nossa condicao é verdadeira");
+        }
+
+        System.out.println("numeroUM é igual a numeroDois " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUM é diferente de numeroDois " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUM é maior de numeroDois " + simNao);
+        */
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos 
+        //estudar a classe String que representa texto na aplicacao
+        
+        String meuNome = "Jorginho Vigas";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short NumeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    
     }
+
+
+
 }
